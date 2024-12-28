0c4f7d4c6b9654707c4672ba1f009832e7caa5ba
Jedrik97 <156681308+Jedrik97@users.noreply.github.com>
2024-12-28 11:37:41 +0300

    Передвижение по мышке и по клаве

---
+++ RPG_Prjct/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,66 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.TextCore.Text;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 700f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Camera _camera;
+
+    private bool _isGrounded;
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        Movement();
+        
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");     
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+        if (movement.magnitude >= 0.1f && _isGrounded)
+        {
+            Vector3 moveDirection = Quaternion.Euler(0, _camera.transform.rotation.eulerAngles.y, 0) * movement;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+            
+            _controller.Move(moveDirection * movementSpeed * Time.deltaTime);
+        }
+
+        if (!_isGrounded)
+        {
+            _velocity.y += gravity * Time.deltaTime;
+        }
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+            _isGrounded = _controller.isGrounded;
+
+            if (_isGrounded && _velocity.y < 0)
+            {
+                _velocity.y = -2f;
+            }
+
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+                
+            }
+            
+    }
+}
---
+++ RPG_Prjct/Assets/Scripts/MouseMovementController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class MouseMovementController : MonoBehaviour
+{
+   [SerializeField] private float _moveSpeed = 5f;
+   [SerializeField] private Camera _camera;
+
+   private Rigidbody _rb;
+   private Vector3 _targetPosition;
+   private bool _isMoving; 
+
+   private void Awake()
+   {
+      _rb = GetComponent<Rigidbody>();
+      _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+   }
+
+   private void Update()
+   {
+      HandleMouseInput();
+   }
+
+   private void FixedUpdate()
+   {
+      if (_isMoving)
+      {
+         HandleMovement();
+      }
+      
+   }
+
+   private void HandleMovement()
+   {
+      Vector3 direction = (_targetPosition - transform.position).normalized;
+      Vector3 velocity = direction * _moveSpeed;
+
+      if (Vector3.Distance(transform.position, _targetPosition) < 0.1f)
+      {
+         _isMoving = false;
+         _rb.linearVelocity = Vector3.zero;
+         return;
+      }
+      _rb.linearVelocity = new Vector3(velocity.x, _rb.linearVelocity.y, velocity.z);
+      Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+      _rb.rotation = Quaternion.RotateTowards(_rb.rotation, targetRotation, _moveSpeed * Time.deltaTime);
+   }
+
+   private void HandleMouseInput()
+   {
+      if (Input.GetMouseButtonDown(0))
+      {
+         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+         {
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+               _targetPosition = hit.point;
+               _isMoving = true; 
+            }
+         }
+      }
+   }
+}
