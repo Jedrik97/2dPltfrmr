282703a0d7a3f4470fd17dcee86f51ee571aef8b
Jedrik97 <156681308+Jedrik97@users.noreply.github.com>
2024-12-28 18:50:40 +0300

    Camera+MouseMovementController

--- RPG_Prjct/Assets/Scripts/MouseMovementController.cs
+++ RPG_Prjct/Assets/Scripts/MouseMovementController.cs
@@ -1,62 +1,68 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MouseMovementController : MonoBehaviour
 {
-   [SerializeField] private float _moveSpeed = 5f;
-   [SerializeField] private Camera _camera;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Camera _camera;
 
-   private Rigidbody _rb;
-   private Vector3 _targetPosition;
-   private bool _isMoving; 
+    private Rigidbody _rb;
+    private NavMeshAgent _agent;
+    
+    private Vector3 _targetPosition;
+    private bool _isMoving;
 
-   private void Awake()
-   {
-      _rb = GetComponent<Rigidbody>();
-      _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-   }
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        _agent = GetComponent<NavMeshAgent>();
+    }
 
-   private void Update()
-   {
-      HandleMouseInput();
-   }
+    private void Update()
+    {
+        HandleMouseInput();
+    }
 
-   private void FixedUpdate()
-   {
-      if (_isMoving)
-      {
-         HandleMovement();
-      }
-      
-   }
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+          //  HandleMovement();
+        }
+    }
 
-   private void HandleMovement()
-   {
-      Vector3 direction = (_targetPosition - transform.position).normalized;
-      Vector3 velocity = direction * _moveSpeed;
+    private void HandleMovement()
+    {
+        Vector3 direction = (_targetPosition - transform.position).normalized;
+        Vector3 velocity = direction * _moveSpeed;
+        
+        if (Vector3.Distance(transform.position, _targetPosition) < 0.1f)
+        { 
+            _isMoving = false;
+            _rb.linearVelocity = Vector3.zero;
+            return;
+        }
 
-      if (Vector3.Distance(transform.position, _targetPosition) < 0.1f)
-      {
-         _isMoving = false;
-         _rb.linearVelocity = Vector3.zero;
-         return;
-      }
-      _rb.linearVelocity = new Vector3(velocity.x, _rb.linearVelocity.y, velocity.z);
-      Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-      _rb.rotation = Quaternion.RotateTowards(_rb.rotation, targetRotation, _moveSpeed * Time.deltaTime);
-   }
+       
+        
+        _rb.linearVelocity = new Vector3(velocity.x, _rb.linearVelocity.y, velocity.z); 
+        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, targetRotation, _moveSpeed * Time.deltaTime));
+    }
 
-   private void HandleMouseInput()
-   {
-      if (Input.GetMouseButtonDown(0))
-      {
-         Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-         {
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-               _targetPosition = hit.point;
-               _isMoving = true; 
+                _agent.SetDestination(hit.point); 
+                
+                _isMoving = true;
             }
-         }
-      }
-   }
+        }
+    }
 }
